a9b5de61f73eb9c3eac3bd743306c8ab02956d56
andrei.draganoiu <andrei.draganoiu@certent.com>
2019-11-14 11:28:41 +0200

    different size of Womens and Mens components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./meniu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
   <div
     style={{
       backgroundImage: `url(${imageUrl})`
     }}
-    className="menu-item"
+    className={`${size} menu-item`}
   >
     <div className="content">
       <h1 className="title">{title}</h1>
