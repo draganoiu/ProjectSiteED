ee622299c23ebeea2b94be4f3635c8a4990a1ddd
andrei.draganoiu <andrei.draganoiu@certent.com>
2019-11-19 10:21:50 +0200

    fixing the /shop

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,26 +1,18 @@
-import React from 'react'
-import './collection-preview.styles.scss'
-import CollectionItem from '../../components/collection-item/collection-item.component'
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPreview = ({title,  items}) => (
-    <div className = 'collection-preview'>
-        <h1 className='Title'>{title.toUpperCase()}</h1>
-        <div className = 'preview'>
-
-           { items
-            .filter((item,idx) => (idx < 4))
-            .map(item =>(
-            
-            <div key={item.id}  {item.price} />
-            
-            ) )}
-        </div>
-        
-    </div>        
-    
-    
-    
-)
-
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="Title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((items, idx) => idx < 4)
+        .map(({ id, ...otherItems }) => (
+          <CollectionItem key={id} {...otherItems} />
+        ))}
+    </div>
+  </div>
+);
 
 export default CollectionPreview;
